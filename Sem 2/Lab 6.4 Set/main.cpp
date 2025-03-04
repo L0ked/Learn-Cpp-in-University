6ab931dcac18ccc80a439813778cf7b4633988a5
Vladimir Pertachkoff <100261267+L0ked@users.noreply.github.com>
2025-03-04 22:02:10 +0300

    Add files via upload

--- Sem 2/Lab 6.4 Set/main.cpp
+++ Sem 2/Lab 6.4 Set/main.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <set>
 
+using namespace std;
+
+struct Answer {
+    int id, answer;
+    bool operator<(const Answer& other) const {
+        return id < other.id; // для сэта, так как он сортирует
+    }
+};
+
+int main() {
+    int id, answer;
+    set<int> mass;
+    set<Answer> pair;
+    cout << "For Break enter 0\n";
+    while (true) {
+        cin >> id;
+        if (id <= 0)
+            break;
+        cin >> answer;
+        if (!mass.contains(id)) {
+            mass.insert(id);
+            pair.insert(Answer(id,answer));
+        }
+    }
+    cout << "SAVED:\n";
+    for (Answer i : pair) {
+        cout << i.id << " " << i.answer << "\n";
+    }
+    return 0;
+}
