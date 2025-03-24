ba441825e0954eebafe4ec5d4f43e96f6021ba9f
Vladimir Pertachkoff <100261267+L0ked@users.noreply.github.com>
2025-03-24 12:48:01 +0300

    Add files via upload

--- Sem 2/Lab 6.6 Map/main.cpp
+++ Sem 2/Lab 6.6 Map/main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <map>
 
+using namespace std;
+
+int main() {
+    string input;
+    getline(cin, input);
+    input.append(" ");
+    int start = 0;
+    map<string, int> slov;
+    for (int i = 0; i < input.length(); i++) {
+        if (i == input.length() - 1 or input[i] == ' ') {
+            string a = input.substr(start, i - start);
+            if (slov.contains(a)) {
+                ++slov[a];
+            }else {
+                slov[a] = 1;
+            }
+            start = ++i;
+        }
+    }
+    for (pair<string,int> i: slov) {
+        cout << i.first << ' ' << i.second << '\n';
+    }
+
+    return 0;
+}
